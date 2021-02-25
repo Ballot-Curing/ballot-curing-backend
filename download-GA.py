033969737c40d868e8cfa0e995ea0a417a903685
Brice Halder <bhalder2@jhu.edu>
2021-02-25 14:41:46 -0500

    update to new schema

--- download-GA.py
+++ download-GA.py
@@ -91,7 +91,7 @@ os.remove(new_file_path) # delete zip file
 # TODO rm all non-STATEWIDE.csv files
 print(f'Files located at: {new_file_dir}')
 
-# database setup
+database setup
 mydb = MySQLdb.connect(host=config['DATABASE']['host'],
     user=config['DATABASE']['user'],
     passwd=config['DATABASE']['passwd'],
@@ -102,11 +102,32 @@ cursor = mydb.cursor()
 
 query = f'''
 CREATE TABLE IF NOT EXISTS {config['GA']['table']} (
-  proc_date DATETIME,
-  county TEXT,
-  voter_registration_no INT,
-  ballot_status VARCHAR(1),
-  status_reason TEXT
+  proc_date               DATETIME,
+  county                  VARCHAR(25),
+  voter_reg_num           INT,
+  first_name              VARCHAR(50),
+  middle_name             VARCHAR(50),
+  last_name               VARCHAR(50),
+  race                    VARCHAR(50),
+  ethnicity               VARCHAR(50),
+  gender                  VARCHAR(10),
+  age                     INT,
+  street_address          VARCHAR(255),
+  city                    VARCHAR(50),
+  state                   VARCHAR(10),
+  zip                     VARCHAR(10),
+  election_dt             DATETIME,
+  party_code              VARCHAR(10),
+  precinct                VARCHAR(50),
+  cong_dist               VARCHAR(50),
+  st_house                VARCHAR(50),
+  st_senate               VARCHAR(50),
+  ballot_style            VARCHAR(50),
+  ballot_req_dt           DATETIME,
+  ballot_send_dt         	DATETIME,
+  ballot_ret_dt	          DATETIME,
+  ballot_issue            VARCHAR(255),
+  ballot_rtn_status       VARCHAR(50)
 );
 '''
 
@@ -118,12 +139,16 @@ LOAD DATA LOCAL INFILE '{csv_file}'
 INTO TABLE {config['GA']['table']}
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS (county, voter_registration_no, @proc_date, @dummy, @dummy, @dummy, @dummy,
-           @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy,
-               @dummy, @dummy, @dummy, ballot_status, status_reason, @dummy, @dummy, @dummy,
-               @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy,
-               @dummy, @dummy, @dummy, @dummy, @dummy)
-               SET proc_date=NOW();
+IGNORE 1 ROWS (county, voter_reg_num, last_name, first_name, middle_name, @dummy, @street_no,
+           @street_name, @apt_no, city, state, zip, @dummy, @dummy, @dummy, @dummy,
+               @dummy, @dummy, @dummy, ballot_rtn_status, ballot_issue, @req_dt, @send_dt,
+               @ret_dt, ballot_style, @dummy, @dummy, @dummy, @dummy, precinct, cong_dist, 
+               st_senate, st_house, @dummy, @dummy, @dummy, @dummy, @dummy, party_code)
+               SET proc_date=NOW(),
+                   street_address = CONCAT(@street_no, ' ', @street_name, ' ', @apt_no),
+                   ballot_req_dt = STR_TO_DATE(@req_dt, '%m/%d/%Y'),
+                   ballot_send_dt = STR_TO_DATE(@send_dt, '%m/%d/%Y'),
+                   ballot_ret_dt = STR_TO_DATE(@ret_dt, '%m/%d/%Y');
 '''
 
 print('Database query started.')
