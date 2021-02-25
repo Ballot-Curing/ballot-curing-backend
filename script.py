46e4b1964661984e482c5f3575b1828014642485
Brice Halder <bhalder2@jhu.edu>
2021-02-25 13:35:21 -0500

    insertion into database

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import shutil
 import configparser
 import time
 import zipfile
+import MySQLdb
 
 from datetime import date 
 from selenium.webdriver import Chrome
@@ -13,15 +14,15 @@ from selenium.webdriver.support.select import Select
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-year = config.get('GA', 'Year')
-name = config.get('GA', 'Name')
+year = config['GA']['year']
+name = config['GA']['name']
 
 # start selenium webdriver
 op = ChromeOptions()
 op.add_argument('--headless')
 browser = Chrome(options=op)
 browser.get(('https://elections.sos.ga.gov/Elections/voterabsenteefile.do'))
-print('Selenium driver started...')
+print('Selenium driver started.')
 
 # select year
 select_election_year = browser.find_element_by_id('nbElecYear')
@@ -29,7 +30,7 @@ for option in select_election_year.find_elements_by_tag_name('option'):
   if option.text == year:
     option.click()
     break
-print('Year selected...')
+print('Year selected.')
 
 # wait 10 seconds for election year data to load
 WebDriverWait(browser, 10)
@@ -40,7 +41,7 @@ for option in select_election_name.find_elements_by_tag_name('option'):
   if option.text == name:
     option.click()
     break
-print('Election selected...')
+print('Election selected.')
 
 # wait 10 seconds for election data to load
 WebDriverWait(browser, 10)
@@ -49,12 +50,12 @@ WebDriverWait(browser, 10)
 url = 'javascript:downLoadFile();'
 download = browser.find_element_by_xpath('//a[@href="'+url+'"]')
 download.click()
-print('File download started...')
+print('File download started.')
 
 # download filename can either be set in config
 # or programmed in script i.e. grab from .crdownload
 # or find pattern from SOS site
-filename = config.get('GA', 'Filename')
+filename = config['GA']['filename']
 
 download_path = config['SYSTEM']['download_dir']
 
@@ -72,19 +73,62 @@ while not os.path.exists(file_path):
 if not os.path.isfile(file_path):
   raise ValueError("%s isn't a file!" % file_path)
 
-storage_path = config.get('GA', 'storage_dir')
+storage_path = config['GA']['storage_dir']
 today = date.today().strftime('%Y-%m-%d')
 
 new_file_path = storage_path + filename
 shutil.move(file_path, new_file_path)
 
 new_file_dir = os.path.join(storage_path, today)
-os.mkdir(new_file_dir)
+if not os.path.exists(new_file_dir):
+  os.mkdir(new_file_dir)
 
 with zipfile.ZipFile(new_file_path, 'r') as zip_ref:
     zip_ref.extractall(new_file_dir)
 
-os.remove(new_file_path)
+os.remove(new_file_path) # delete zip file
 
 # TODO rm all non-STATEWIDE.csv files
 print(f'Files located at: {new_file_dir}')
+
+# database setup
+mydb = MySQLdb.connect(host=config['DATABASE']['host'],
+    user=config['DATABASE']['user'],
+    passwd=config['DATABASE']['passwd'],
+    db=config['GA']['db'],
+    local_infile = 1)
+
+cursor = mydb.cursor()
+
+query = f'''
+CREATE TABLE IF NOT EXISTS {config['GA']['table']} (
+  proc_date DATETIME,
+  county TEXT,
+  voter_registration_no INT,
+  ballot_status VARCHAR(1),
+  status_reason TEXT
+);
+'''
+
+cursor.execute(query)
+csv_file = os.path.join(new_file_dir, config['GA']['csv_name'])
+
+query =f'''
+LOAD DATA LOCAL INFILE '{csv_file}'
+INTO TABLE {config['GA']['table']}
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS (county, voter_registration_no, @proc_date, @dummy, @dummy, @dummy, @dummy,
+           @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy,
+               @dummy, @dummy, @dummy, ballot_status, status_reason, @dummy, @dummy, @dummy,
+               @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy,
+               @dummy, @dummy, @dummy, @dummy, @dummy)
+               SET proc_date=NOW();
+'''
+
+print('Database query started.')
+
+cursor.execute(query)
+mydb.commit()
+
+print('Database query executed.')
