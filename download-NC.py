a8d6f6b06906d75974c6b41a7b751e2de543ef3b
Nicholas Bowen <nbowen3@jhu.edu>
2021-02-28 12:03:21 -0500

    added ability to unzip and delete zip file

--- download-NC.py
+++ download-NC.py
@@ -1,4 +1,6 @@
 import requests
+import zipfile
+import os
 
 # url of zip and target zip name
 url = 'https://s3.amazonaws.com/dl.ncsbe.gov/ENRS/2020_11_03/absentee_20201103.zip'
@@ -7,3 +9,12 @@ target_file = 'ncdata.zip'
 # get file and write it to output 
 myfile = requests.get(url)
 open(target_file, 'wb').write(myfile.content)
+
+# unzips file
+with zipfile.ZipFile(target_file, 'r') as zip_ref:
+  print('Unzipping file: ' + target_file)
+  zip_ref.extractall()
+
+# deletes the old zip file
+print('Deleting old zip file')
+os.remove(target_file)
