bf183bfece1e8c0e5e1b52ed48b3651b74a6a0b4
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2021-03-30 16:15:29 -0400

    Add skeleton code for API endpoints

--- api/src/lastProcessed.py
+++ api/src/lastProcessed.py
@@ -1,7 +1,19 @@
 from flask import Blueprint
+import MySQLdb
 
 lastProcessed_bp = Blueprint('lastProcessed', __name__)
 
 @lastProcessed_bp.route('/lastProcessed')
 def lastProcessed():
+  
+  state = "ga" # temp
+  
+  mydb = MySQLdb.connect(host=config['DATABASE']['host'],
+    user=config['DATABASE']['user'],
+    passwd=config['DATABASE']['passwd'],
+    db="vote_"+state, 
+    local_infile = 1)
+  
+  # query `election` table for last proc_dt
   return "Last processed: today"
+  
