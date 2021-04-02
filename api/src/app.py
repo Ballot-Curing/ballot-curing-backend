53c4648b70741190da345fc49c88e5340046c581
Nick Bowen <nbowen3@jhu.edu>
2021-04-02 11:47:42 -0400

    updated API url and fixed comment

--- api/src/app.py
+++ api/src/app.py
@@ -3,8 +3,8 @@ from lastProcessed import lastProcessed_bp
 from ballots import ballots_bp
 
 app = Flask(__name__)
-app.register_blueprint(lastProcessed_bp, url_prefix='/api/lastProcessed')
-app.register_blueprint(ballots_bp, url_prefix='/api/ballots')
+app.register_blueprint(lastProcessed_bp, url_prefix='/api/v1/lastProcessed')
+app.register_blueprint(ballots_bp, url_prefix='/api/v1/ballots')
 
 @app.route('/')
 def hello_world():
