98736e6e79be950814efa5cdf63300c227b3af55
Nick Bowen <nbowen3@jhu.edu>
2021-04-17 22:22:43 -0400

    got working version with updated stats

--- api/src/stats.py
+++ api/src/stats.py
@@ -1,5 +1,6 @@
 import MySQLdb
 import configparser
+import json
 
 from flask import Blueprint
 from flask import abort
@@ -38,14 +39,39 @@ def stats():
     
     cursor.execute(query)
 
-    rows = cursor.fetchall()
+    row = cursor.fetchone()
 
-    for row in rows:
-        election_dt = row['election_dt']
-        tot_rej = row['tot_rejected']
-        tot_cured = row['tot_cured']
-        tot_processed = row['tot_processed']
-        break
+    # gets the fields
+    election_dt = row['election_dt']
+    tot_rej = row['tot_rejected']
+    tot_cured = row['tot_cured']
+    tot_processed = row['tot_processed']
+
+    # string parsing to convert gender_rej into the right form
+    gender_rej = json.loads(row['gender_rej'])
+    gender_rej = process_string(gender_rej)
+
+    # string parsing to convert race_rej into the right form
+    race_rej = json.loads(row['race_rej'])
+    race_rej = race_rej.replace('(', "[")
+    race_rej = race_rej.replace(')', "]")
+    race_rej = race_rej.replace("'", '"') 
+    race_rej = race_rej.replace(', {"race": ""UNDESIGNATED", "race_count": 2}', "") # TODO: Add 2 to the normal undesignated count to account for this
+    race_rej = json.loads(race_rej)
+
+    # converts age_rej into the correct form
+    age_rej = json.loads(row['age_rej'])
+    age_rej = age_rej.replace("Decimal('", "")
+    age_rej = age_rej.replace("')", "")
+    age_rej = age_rej.replace("},)", "})")
+    age_rej = age_rej.replace('(', "[")
+    age_rej = age_rej.replace(')', "]")
+    age_rej = age_rej.replace("'", '"')
+    age_rej = json.loads(age_rej)
+
+    # string parsing to convert rej_reason into the right form
+    rej_reason = json.loads(row['rej_reason'])
+    rej_reason = process_string(rej_reason) # can use general method as no special cases
 
     query = f'''
     SELECT *
@@ -71,6 +97,10 @@ def stats():
 	    "election_dt" : election_dt.strftime("%m/%d/%Y"),
         "total_rejected" : tot_rej,
         "total_cured" : tot_cured,
+        "rejected_gender" : gender_rej,
+        "rejected_race" : race_rej,
+        "rejected_age_group" : age_rej,
+        "ballot_issue_count" : rej_reason,
         "county_rejected" : county_reject,
         "county_cured" : county_cured,
         "county_processed" : county_processed
@@ -79,4 +109,12 @@ def stats():
     response = jsonify(ret)
     response.headers.add('Access-Control-Allow-Origin', '*')
 
+    return response
+
+# method to process a json response from sql to valid json form 
+def process_string(response):
+    response = response.replace('(', "[")
+    response = response.replace(')', "]")
+    response = response.replace("'", '"')
+    response = json.loads(response)
     return response
