536e40905345505ff4daa717c49ea9ddae61c5d9
yzhan289 <andrewzhang25@gmail.com>
2021-04-26 19:16:25 -0400

    Add find_cured to insertGAfiles

--- db/scripts/find_cured.py
+++ db/scripts/find_cured.py
@@ -61,7 +61,7 @@ def find_cured_NC(state):
 
 
 # Adds entries to cured and rejected tables
-def find_cured(today_datetime, state):
+def find_cured(state):
   mydb = MySQLdb.connect(host=config['DATABASE']['host'],
               user=config['DATABASE']['user'],
               passwd=config['DATABASE']['passwd'],
@@ -73,68 +73,63 @@ def find_cured(today_datetime, state):
 
   elections = get_elections(cursor)
   
-  # for election in elections:
-  
-  election = "2021_01_01"
-  
-  cured_db = f'cured_{election}'
-  rejected_db = f'rejected_{election}'
+  for election in elections:
+    cured_db = f'cured_{election}'
+    rejected_db = f'rejected_{election}'
 
-  # make cured table if not made
-  cursor.execute(schema_table(cured_db))
+    # make cured table if not made
+    cursor.execute(schema_table(cured_db))
 
-  # make rejected table if not made
-  cursor.execute(schema_table(rejected_db))
+    # make rejected table if not made
+    cursor.execute(schema_table(rejected_db))
 
-  # get all cured ballots as of today by comparing rejected_db and accepted ballots in election table
-  print("Get all cured ballots")
-  cursor.execute(queries.get_cured(election, rejected_db))
-  output = cursor.fetchall()
+    # get all cured ballots as of today by comparing rejected_db and accepted ballots in election table
+    print("Get all cured ballots")
+    cursor.execute(queries.get_cured(election, rejected_db))
+    output = cursor.fetchall()
 
-  # for each cured entry, add to cured_db and remove from rejected_db
-  for entry in output:
-    cursor.execute(schema.add_to_cured(cured_db, entry))
-    cursor.execute(schema.remove_cured_from_rejected(rejected_db, entry))
+    # for each cured entry, add to cured_db and remove from rejected_db
+    for entry in output:
+      cursor.execute(schema.add_to_cured(cured_db, entry))
+      cursor.execute(schema.remove_cured_from_rejected(rejected_db, entry))
+      mydb.commit()
+
+    # query the current day for any new rejected that are not cured
+    print("Getting today's rejected ballots from main table")
+    cursor.execute(queries.get_rejected(election, cured_db))
+    output = cursor.fetchall()
+
+    print("Adding rejected ballots to rejected table")
+    # for each rejected entry today, add to rejected table
+    for entry in output:
+      try:
+        cursor.execute(schema.add_to_rejected(rejected_db, entry))
+      except:
+        print("Error adding the following entry: ")
+        print(entry)
+        continue
+
+    # commit changes to db when done with election
     mydb.commit()
-
-  # query the current day for any new rejected that are not cured
-  print("Getting today's rejected ballots from main table")
-  cursor.execute(queries.get_rejected(election, cured_db))
-  output = cursor.fetchall()
-
-  print("Adding rejected ballots to rejected table")
-  # for each rejected entry today, add to rejected table
-  for entry in output:
-    try:
-      cursor.execute(schema.add_to_rejected(rejected_db, entry))
-    except:
-      continue
-    
-
-  # commit changes to db when done with election
-  mydb.commit()
 
   # To close the connection
   mydb.close()
 
+# Helper function if running from another program
+def run_find_cured(state):
+  if state == "NC":
+    find_cured_NC("NC")
+  else:
+    find_cured(state)
 
-# Driver Code
+# Main code if running from terminal
 if __name__ == "__main__":
 
   if len(sys.argv) > 1 and sys.argv[1] == "NC":
-    find_cured_NC("NC")
+    run_find_cured("NC")
   elif len(sys.argv) > 1:
     # TODO: Remove this for-loop for real election
-    start_date = "10/10/20" 
-    start_datetime = datetime.strptime(start_date, '%m/%d/%y')
-    for i in range(95):
-      print("Start date: " + str(start_datetime))
-      find_cured(start_datetime, sys.argv[1])
-      start_datetime += timedelta(days=1)
+    run_find_cured(sys.argv[1])
   else: # By default just do GA
-    start_date = "1/1/21" 
-    start_datetime = datetime.strptime(start_date, '%m/%d/%y')
-    for i in range(1):
-      print("Start date: " + str(start_datetime))
-      find_cured(start_datetime, "GA")
-      start_datetime += timedelta(days=1)
+    run_find_cured("GA")
+
